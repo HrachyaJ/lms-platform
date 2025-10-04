cbb150629c6bd70ede40e4ed9e0e53aacb713f86
Hrachya <hrachjgshow@gmail.com>
2025-10-04 12:26:25 +0400

    first commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Companion Session</div>;
+};
+
+export default page;
---
+++ app/companions/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Companions</div>;
+};
+
+export default page;
