6cac7ccaf55dad674e6e2c47e7ad040dc5f9be58
Hrachya <hrachjgshow@gmail.com>
2025-10-06 17:28:46 +0400

    home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,48 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/shared/CompanionCard";
+import CompanionsList from "@/components/shared/CompanionsList";
+import CTA from "@/components/shared/CTA";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to Converso</h1>
-      <p className="mt-4 text-lg">Your real-time AI teaching platform.</p>
-      <Button className="mt-6">Get Started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Math"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
---
+++ components/shared/CTA.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const CTA = () => {
+  return (
+    <section className="cta-section">
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalize Learning Companions
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personality - and start learning through
+        voice conversations that feel natural and fun.
+      </p>
+      <Image
+        src="/images/cta.svg"
+        alt="CTA Image"
+        width={362}
+        height={232}
+        className="cta-image"
+      />
+      <button className="btn-primary">
+        <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
+        <Link href="/companion/new">
+          <p>Build a new Companion</p>
+        </Link>
+      </button>
+    </section>
+  );
+};
+
+export default CTA;
